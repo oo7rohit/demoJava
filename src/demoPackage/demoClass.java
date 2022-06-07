1765c4dc14becf552d34cdd1d74066850fb34419
Rohit Kumar Agrawal <agrawal.rohit@BLRETV-C02VN106.local>
2022-06-07 08:46:50 +0530

    first commit

---
+++ src/demoPackage/demoClass.java
@@ -0,0 +1,11 @@
+package demoPackage;
+
+public class demoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is demo Java file");
+
+	}
+
+}
